93069ff2eea721ece301f8afa9613a0be9eb1f3c
inedibleProgrammer <inedibleProgrammer@gmail.com>
2024-05-08 13:14:55 -0400

    Use snake init in main function

--- src/main.c
+++ src/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-
+#include "Snake.h"
 
 int main(int argc, char** argv)
 {
-    printf("Entry\n");
+    Snake_T mainSnake;
+    Snake_Init(&mainSnake);
+    printf("Main snake char: %c\n", mainSnake.headSymbol);
     return 0;
 }
 
