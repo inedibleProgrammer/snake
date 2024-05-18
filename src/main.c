647b59e8aefa676e1c13c386705ee9aa05b6221a
inedibleProgrammer <inedibleProgrammer@gmail.com>
2024-05-17 21:42:26 -0400

    Implement logging with clogger

--- src/main.c
+++ src/main.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "snake.h"
+#include "logger.h"
 
 int main(int argc, char** argv)
 {
     Snake_T mainSnake;
     Snake_Init(&mainSnake);
     printf("Main snake char: %c\n", mainSnake.headSymbol);
+
+    logger_initFileLogger("log.txt", 1024 * 1024, 5);
+    logger_setLevel(LogLevel_DEBUG);
+    LOG_INFO("file logging");
+    LOG_DEBUG("format example: %d%c%s", 1, '2', "3");
+
+    // system("clear");
     return 0;
 }
 
