fb8d67d1ef3d2e5b653afba23aba4561cfb14875
inedibleProgrammer <inedibleProgrammer@gmail.com>
2024-05-24 20:12:09 -0400

    Replace minunit with ctest

--- src/main.c
+++ src/main.c
@@ -11,8 +11,9 @@ int main(int argc, char** argv)
 
     logger_initFileLogger("log.txt", 1024 * 1024, 5);
     logger_setLevel(LogLevel_DEBUG);
-    LOG_INFO("file logging");
-    LOG_DEBUG("format example: %d%c%s", 1, '2', "3");
+    LOG_INFO("file logging init");
+    // LOG_DEBUG("format example: %d%c%s", 1, '2', "3");
+
 
     // system("clear");
     return 0;
